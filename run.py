a26fd89efb0efbb5fa54dad900ca7f46cb0dd91b
Rontoni <rjjjohansson@gmail.com>
2023-01-18 12:50:09 +0000

    Add function for choosing word

--- run.py
+++ run.py
@@ -6,11 +6,17 @@ from random import choice
 
 # Words for our game 
 
-words = ['motivation', 'appointment', 'discovery', 'caligraphy', 'rendering', 'linguistics', 'preposterous']
+words = ['motivation', 'appointment', 'discovery', 'caligraphy', 'rendering',]
 correct_letters = []
 incorrect_letters = []
 tries = 6
 right_answers = 0 
 game_over = False
 
+# Function for starting the game with a word 
 
+def choose_word(list_of_words):
+    chosen_word = choice(list_of_words)
+    different_letters = len(set(chosen_word))
+
+    return chosen_word, different_letters
