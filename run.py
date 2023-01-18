05b18353001e200b3f7b0219afb297a90a82c53d
Rontoni <rjjjohansson@gmail.com>
2023-01-18 13:04:45 +0000

    Add board function

--- run.py
+++ run.py
@@ -13,14 +13,14 @@ tries = 6
 right_answers = 0 
 game_over = False
 
-# Function for choosing a word 
-
+#Function for picking a word 
 def choose_word(list_of_words):
     chosen_word = choice(list_of_words)
     different_letters = len(set(chosen_word))
 
     return chosen_word, different_letters
 
+#Function for picking a letter
 def ask_letter():
     chosen_letter = ''
     is_valid = False
@@ -35,3 +35,13 @@ def ask_letter():
             print("You haven't chosen a correct letter")
     
     return chosen_letter
+
+#Function for showing the board while picking words
+def show_new_board(chosen_word):
+    hidden_list = []
+
+    for l in chosen_word:
+        if l in correct_letters: 
+            hidden_list.append(l)
+        else:
+            hidden_list.append('-')
