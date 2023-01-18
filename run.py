38f33aa9af67a9e0acc903e2018edd79201857bb
Rontoni <rjjjohansson@gmail.com>
2023-01-18 13:52:32 +0000

    Add prints and variables for game interface

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ Hangman Game
 
 from random import choice 
 
-# Words for our game 
+# Words for the game 
 
 words = ['motivation', 'appointment', 'discovery', 'caligraphy', 'rendering',]
 correct_letters = []
@@ -13,14 +13,14 @@ tries = 6
 right_answers = 0 
 game_over = False
 
-#Function for picking a word 
+# Function for picking a word 
 def choose_word(list_of_words):
     chosen_word = choice(list_of_words)
     different_letters = len(set(chosen_word))
 
     return chosen_word, different_letters
 
-#Function for picking a letter
+# Function for picking a letter
 def ask_letter():
     chosen_letter = ''
     is_valid = False
@@ -36,7 +36,7 @@ def ask_letter():
     
     return chosen_letter
 
-#Function for showing the board while picking words
+# Function for showing the board while picking words
 def show_new_board(chosen_word):
     hidden_list = []
 
@@ -47,7 +47,8 @@ def show_new_board(chosen_word):
             hidden_list.append('-')
     print(' '.join(hidden_list))
 
-#Function for checking if the letter entered by the player matches the hidden word and tries left
+# Function for checking if the letter entered and matching words.
+# Also attempts left. 
 def check_letter(chosen_letter, hidden_word, tries, matches):
     end = False 
 
@@ -64,3 +65,31 @@ def check_letter(chosen_letter, hidden_word, tries, matches):
         end = win(hidden_word)
     
     return tries, end, matches
+
+# Function for loosing the game
+def lose():
+    print("You don't have any tries left")
+    print("The hidden word was " + word)
+
+    return True
+
+#Function for winning the game 
+def win(revealed_word):
+    show_new_board(revealed_word)
+    print("Congratulations, you guessed correctly!")
+
+    return True 
+
+#
+word, unique_letters = choose_word(words)
+
+while not game_over:
+    print('\n' + '*' * 20 + '\n')
+    show_new_board(word)
+    print('\n')
+    print('Incorrect letters: ' + '-'.join(incorrect_letters))
+    print(f'Tries: {tries}')
+    print('\n' + '*' * 20 + '\n')
+    letter = ask_letter()
+    tries, over, right_answers = check_letter(letter, word, tries, right_answers)
+    game_over = over
