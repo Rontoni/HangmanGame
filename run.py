8473f5e2b5296f8b27f3df9b5c59f408c426f58d
Rontoni <rjjjohansson@gmail.com>
2023-01-18 23:39:50 +0000

    Adjust code after validation

--- run.py
+++ run.py
@@ -1,25 +1,30 @@
 """
-Hangman Game 
+Hangman Game
 """
 
-from random import choice 
+from random import choice
 
-# Words for the game 
-words = ['motivation', 'appointment', 'discovery', 'caligraphy', 'rendering',]
+# Words for the game
+words = ['motivation', 'appointment', 'discovery', 'caligraphy', 'rendering']
 correct_letters = []
 incorrect_letters = []
 tries = 6
-right_answers = 0 
+right_answers = 0
 game_over = False
 
-# Function for picking a word 
+# Function for picking a word
+
+
 def choose_word(list_of_words):
     chosen_word = choice(list_of_words)
     different_letters = len(set(chosen_word))
 
     return chosen_word, different_letters
 
+
 # Function for picking a letter
+
+
 def ask_letter():
     chosen_letter = ''
     is_valid = False
@@ -29,57 +34,67 @@ def ask_letter():
         chosen_letter = input("Please choose a letter:")
 
         if chosen_letter in alphabet and len(chosen_letter) == 1:
-            is_valid = True 
-        else: 
+            is_valid = True
+        else:
             print("You haven't chosen a correct letter")
-    
+
     return chosen_letter
 
 # Function for showing the board while picking words
+
+
 def show_new_board(chosen_word):
     hidden_list = []
 
-    for l in chosen_word:
-        if l in correct_letters: 
-            hidden_list.append(l)
+    for c in chosen_word:
+        if c in correct_letters:
+            hidden_list.append(c)
         else:
             hidden_list.append('-')
     print(' '.join(hidden_list))
 
 # Function for checking if the letter entered and matching words.
-# Also attempts left. 
+# Also attempts left.
+
+
 def check_letter(chosen_letter, hidden_word, tries, matches):
-    end = False 
+    end = False
 
     if chosen_letter in hidden_word:
         correct_letters.append(chosen_letter)
-        matches += 1 
+        matches += 1
     else:
         incorrect_letters.append(chosen_letter)
-        tries -= 1 
-    
+        tries -= 1
+
     if tries == 0:
         end = lose()
     elif matches == unique_letters:
         end = win(hidden_word)
-    
+
     return tries, end, matches
 
 # Function for loosing the game
+
+
 def lose():
     print("You don't have any tries left")
     print("The hidden word was " + word)
 
     return True
 
-# Function for winning the game 
+# Function for winning the game
+
+
 def win(revealed_word):
     show_new_board(revealed_word)
     print("Congratulations, you guessed correctly!")
 
-    return True 
+    return True
 
 # Interface for the game in the terminal
+
+
 word, unique_letters = choose_word(words)
 
 while not game_over:
@@ -90,6 +105,6 @@ while not game_over:
     print(f'Tries: {tries}')
     print('\n' + '*' * 20 + '\n')
     letter = ask_letter()
-    tries, over, right_answers = check_letter(letter, word, tries, 
-    right_answers)
+    tries, over, right_answers = check_letter(letter, word,
+                                              tries, right_answers)
     game_over = over
