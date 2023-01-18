c4b0657095ca0eb9ea6aa7d3dd3e0512bd0a2473
Rontoni <rjjjohansson@gmail.com>
2023-01-18 12:55:15 +0000

    Add function for choosing a letter

--- run.py
+++ run.py
@@ -13,10 +13,25 @@ tries = 6
 right_answers = 0 
 game_over = False
 
-# Function for starting the game with a word 
+# Function for choosing a word 
 
 def choose_word(list_of_words):
     chosen_word = choice(list_of_words)
     different_letters = len(set(chosen_word))
 
     return chosen_word, different_letters
+
+def ask_letter():
+    chosen_letter = ''
+    is_valid = False
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    while not is_valid:
+        chosen_letter = input("Please choose a letter")
+
+        if chosen_letter in alphabet and len(chosen_letter) == 1:
+            is_valid = True 
+        else: 
+            print("You haven't chosen a correct letter")
+    
+    return chosen_letter
